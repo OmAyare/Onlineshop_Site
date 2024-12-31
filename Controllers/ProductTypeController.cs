e030ba30e62f298d0d62fad82e54310e70f4733e
OmAyare <omayare13@gmail.com>
2024-12-31 21:50:11 +0530

    Add project files.

---
+++ Controllers/ProductTypeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Purchase.Models;
+
+namespace Purchase.Controllers
+{
+    public class ProductTypeController : Controller
+    {
+        // GET: ProductType
+        public ActionResult List()
+        {
+            List<ProductType> productTypeList = new List<ProductType>()
+            {
+                new ProductType (){pk_prodtypeId=1, Description="Open Product"},
+                new ProductType (){pk_prodtypeId=2, Description="CLose Product"},
+                new ProductType (){pk_prodtypeId=3, Description="Cloth Product"}
+            };
+            return View(productTypeList);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Purchase.Startup))]
+namespace Purchase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
